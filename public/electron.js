1a9601f13301d01c4b80b0fbad8ccc04a6e64d21
Tohaker <milesbardon@gmail.com>
2020-05-22 10:31:53 +0100

    Moving server to public folder

--- public/electron.js
+++ public/electron.js
@@ -4,7 +4,7 @@ const { autoUpdater } = require("electron-updater");
 const path = require("path");
 const url = require("url");
 
-require("../server");
+require("./server");
 
 function createWindow() {
   // Create the browser window.
