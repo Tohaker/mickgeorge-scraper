ba3c04f618a641f5895229e5a19679b3983b50d9
Tohaker <milesbardon@gmail.com>
2020-05-23 22:40:53 +0100

    Changing to HashRouter for Electron to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import LoginContainer from "./Login";
-import Companies from "./Companies";
+import LoginContainer from "./login";
+import Companies from "./companies";
 
 const App = () => {
   const [companies, setCompanies] = React.useState([]);
