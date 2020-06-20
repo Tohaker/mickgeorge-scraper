944a0da4c10bf9a19d881bb020282b68de4b679a
Tohaker <milesbardon@gmail.com>
2020-06-20 10:23:34 +0100

    Updating urlAction tests

--- src/components/urlAction/urlAction.test.tsx
+++ src/components/urlAction/urlAction.test.tsx
@@ -1,44 +1,108 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import editIcon from "#/assets/edit-white.png";
+import tickIcon from "#/assets/tick-white.png";
 import UrlAction from ".";
 
 describe("URL Action", () => {
   const props = {
     add: jest.fn(),
     remove: jest.fn(),
+    update: jest.fn(),
   };
 
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
-  it("should render with add button", () => {
-    const { getByText } = render(<UrlAction {...props} url="" />);
+  describe("given the add button is clicked", () => {
+    let addButton: HTMLElement;
+    let textBox: HTMLElement;
 
-    expect(getByText("+")).toBeInTheDocument();
+    beforeEach(() => {
+      const { getByText, getByRole } = render(<UrlAction {...props} url="" />);
+      addButton = getByText("+");
+      textBox = getByRole("textbox");
+    });
+
+    it("should call add", () => {
+      expect(addButton).toBeInTheDocument();
+
+      fireEvent.change(textBox, { target: { value: "text" } });
+      fireEvent.click(addButton);
+
+      expect(props.add).toHaveBeenCalledWith("text");
+    });
   });
 
-  it("should call add with add button", () => {
-    const { getByText, getByRole } = render(<UrlAction {...props} url="" />);
+  describe("given the remove button is clicked", () => {
+    let removeButton: HTMLElement;
 
-    const input = getByRole("textbox");
-    fireEvent.change(input, { target: { value: "text" } });
-    fireEvent.click(getByText("+"));
+    beforeEach(() => {
+      const { getByText } = render(<UrlAction {...props} url="url" />);
+      removeButton = getByText("-");
+    });
 
-    expect(props.add).toHaveBeenCalledWith("text");
+    it("should call remove", () => {
+      expect(removeButton).toBeInTheDocument();
+
+      fireEvent.click(removeButton);
+
+      expect(props.remove).toHaveBeenCalledWith("url");
+    });
   });
 
-  it("should render with remove button", () => {
-    const { getByText } = render(<UrlAction {...props} url="url" />);
+  describe("given the edit button is clicked", () => {
+    let editButton: HTMLElement;
+    let textBox: HTMLElement;
 
-    expect(getByText("-")).toBeInTheDocument();
-  });
+    beforeEach(() => {
+      const { getByAltText, getByRole } = render(
+        <UrlAction {...props} url="url" />
+      );
+      editButton = getByAltText("Edit");
+      textBox = getByRole("textbox");
+    });
 
-  it("should call remove with remove button", () => {
-    const { getByText } = render(<UrlAction {...props} url="url" />);
+    it("should change the icon", () => {
+      expect(editButton).toHaveAttribute("src", editIcon);
 
-    fireEvent.click(getByText("-"));
+      fireEvent.click(editButton);
 
-    expect(props.remove).toHaveBeenCalledWith("url");
+      expect(editButton).toHaveAttribute("src", tickIcon);
+    });
+
+    it("should allow the text to be edited", () => {
+      expect(textBox).toHaveAttribute("readOnly");
+
+      fireEvent.click(editButton);
+
+      expect(textBox).not.toHaveAttribute("readOnly");
+    });
+
+    describe("given a new value is entered", () => {
+      it("should call update", () => {
+        fireEvent.click(editButton);
+        expect(props.update).not.toBeCalled();
+
+        const value = "text";
+        fireEvent.change(textBox, { target: { value } });
+
+        fireEvent.click(editButton);
+        expect(props.update).toBeCalledWith(value, "url");
+        expect(props.update).toBeCalledTimes(1);
+      });
+
+      it("should not call update", () => {
+        fireEvent.click(editButton);
+        expect(props.update).not.toBeCalled();
+
+        const value = "url";
+        fireEvent.change(textBox, { target: { value } });
+
+        fireEvent.click(editButton);
+        expect(props.update).not.toBeCalled();
+      });
+    });
   });
 });
